93c592a2d1b116a398137d9dff681d154724110b
Min RK <benjaminrk@gmail.com>
2025-08-20 14:07:09 -0700

    add even more isolated test

---
+++ test.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+
+def test_xdoctest_debug():
+    import zmq
+
+    with zmq.Context() as ctx:
+        print(f"have {ctx=}")
+        interface = "tcp://127.0.0.1"
+        with ctx.socket(zmq.ROUTER) as server, ctx.socket(zmq.DEALER) as client:
+            server.linger = client.linger = 1_000
+            print("linger")
+            print(server.get(zmq.LINGER))
+            print("binding")
+            port = server.bind_to_random_port(interface)
+            print(f"bound to {port}")
+            url = f"{interface}:{port}"
+            print(f"connecting to {url}")
+            client.connect(url)
+            print("sending")
+            client.send(b"ping")
+            msg = server.recv_multipart()
+            print("recvd", msg, "replying")
+            server.send_multipart(msg)
+            reply = client.recv_multipart()
+            print("recvd reply", reply)
+
+    import nbformat  # NOQA
+    from nbclient import NotebookClient
+
+    notebook_fpath = (
+        Path(__file__).parent.resolve() / "tests" / "notebook_with_doctests.ipynb"
+    )
+    with open(notebook_fpath, "r+") as file:
+        nb = nbformat.read(file, as_version=nbformat.NO_CONVERT)
+    print("creating client")
+    nbc = NotebookClient(nb)
+    print("executing")
+    nb = nbc.execute()
+    print("executed")
+    for cell in nb.cells:
+        if cell.cell_type == "code":
+            for output in cell.outputs:
+                if output.output_type == "stream":
+                    print(output.text)
+
+
+test_xdoctest_debug()
