be190ca8ddef0e7374efa3ac0b40b24b74c4e989
Min RK <benjaminrk@gmail.com>
2025-08-20 21:32:24 -0700

    without kc

--- test.py
+++ test.py
@@ -44,21 +44,21 @@ def test_xdoctest_debug():
     print("executing")
     ctx = zmq.Context.instance()
     print(f"before {ctx._sockets=}")
-    from jupyter_client import AsyncKernelManager
+    from jupyter_client import KernelManager, AsyncKernelManager
     km = AsyncKernelManager()
     print(f"{km=}")
     run_sync(km.start_kernel)()
-    async def make_client():
-        kc = km.client()
-        print(f"{kc=}")
-        kc.start_channels()
-        await kc.wait_for_ready()
-        return kc
-        
-    kc = run_sync(make_client)()
-    print(f"{kc=}")
-    kc.stop_channels()
-    from functools import partial
+    # async def make_client():
+    #     kc = km.client()
+    #     print(f"{kc=}")
+    #     kc.start_channels()
+    #     await kc.wait_for_ready()
+    #     return kc
+    #     
+    # kc = run_sync(make_client)()
+    # print(f"{kc=}")
+    # kc.stop_channels()
+    # from functools import partial
     run_sync(partial(km.shutdown_kernel, now=True))()
     run_sync(km.cleanup_resources)()
     
