50e166bbd21d5bb86e6af526e2010631e138c0e9
Min RK <benjaminrk@gmail.com>
2025-08-21 08:25:34 -0700

    try SelectorEventLoop

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ from contextvars import ContextVar
 
 context_loop = ContextVar("context_loop", default=None)
 
+
 def test_crash():
-    loop = asyncio.new_event_loop()
+    loop = asyncio.SelectorEventLoop()
     context_loop.set(loop)
     # if loop is unclosed, Windows Python 3.14.0rc2 segfaults at exit
     # loop.close()
+
 
 if __name__ == "__main__":
     test_crash()
