5d106f4dd9ea2f60386fb8f17c4722d674c52287
Min RK <benjaminrk@gmail.com>
2025-08-21 08:17:33 -0700

    try without contextvar

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from contextvars import ContextVar
 _loop: ContextVar[asyncio.AbstractEventLoop | None] = ContextVar("_loop", default=None)
 
 def test_crash():
-    _loop.set(asyncio.new_event_loop())
+    local_loop = asyncio.new_event_loop()
+    # _loop.set(asyncio.new_event_loop())
 
 if __name__ == "__main__":
     test_crash()
