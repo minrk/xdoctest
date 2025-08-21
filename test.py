8ea359137944184cd1cd3272d365d751f49ba7bf
Min RK <benjaminrk@gmail.com>
2025-08-21 08:22:22 -0700

    even simpler

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
-from __future__ import annotations
-
 import asyncio
 from contextvars import ContextVar
 
 
-context_loop: ContextVar[asyncio.AbstractEventLoop | None] = ContextVar("context_loop", default=None)
+context_loop = ContextVar("context_loop", default=None)
 
 def test_crash():
     loop = asyncio.new_event_loop()
     context_loop.set(loop)
-    # if loop is unclosed, Windows Python 3.14rc2 segfaults at exit
+    # if loop is unclosed, Windows Python 3.14.0rc2 segfaults at exit
     # loop.close()
 
 if __name__ == "__main__":
     test_crash()
-    print("cleaning up")
+    print("end of script")
