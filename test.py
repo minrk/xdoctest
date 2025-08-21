6fe9a26583d637c5352c83ff45f446d3fc6f8ba4
Min RK <benjaminrk@gmail.com>
2025-08-21 08:20:41 -0700

    don't close, should segfault

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ context_loop: ContextVar[asyncio.AbstractEventLoop | None] = ContextVar("context
 def test_crash():
     loop = asyncio.new_event_loop()
     context_loop.set(loop)
-    loop.close()
+    # if loop is unclosed, Windows Python 3.14rc2 segfaults at exit
+    # loop.close()
 
 if __name__ == "__main__":
     test_crash()
